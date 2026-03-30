850ead2203080a907aec8c55240b9c771a8e3f5a
matsumoto <255082730+matsumoto219@users.noreply.github.com>
2026-03-30 20:39:23 +0900

    支払い方法と品目のカテゴリをモデルに追加

--- app/models/receipt_item.rb
+++ app/models/receipt_item.rb
@@ -1,3 +1,26 @@
+
 class ReceiptItem < ApplicationRecord
+  CATEGORIES = %w[
+    food
+    drink
+    daily_goods
+    household
+    medical
+    beauty
+    transportation
+    hobby
+    other
+  ].freeze
+
   belongs_to :receipt
+
+  def self.category_options
+    CATEGORIES.map do |key|
+      [ I18n.t("enums.receipt_item.category.#{key}"), key ]
+    end
+  end
+
+  def category_label
+    I18n.t("enums.receipt_item.category.#{category}")
+  end
 end
