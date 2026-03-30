0e156c129a41727b97fa3f78814a24a208aa0e92
matsumoto <255082730+matsumoto219@users.noreply.github.com>
2026-03-30 20:39:23 +0900

    支払い方法と品目のカテゴリをモデルに追加

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,7 +1,26 @@
 class Receipt < ApplicationRecord
+  PAYMENT_METHODS = %w[
+    cash
+    credit_card
+    e_money
+    qr_payment
+    debit_card
+    other
+  ].freeze
+
   belongs_to :user
   has_many :receipt_items, dependent: :destroy
   has_one_attached :image
 
   accepts_nested_attributes_for :receipt_items, allow_destroy: false
+
+  def self.payment_method_options
+    PAYMENT_METHODS.map do |key|
+      [ I18n.t("enums.receipt.payment_method.#{key}"), key ]
+    end
+  end
+
+  def payment_method_label
+    I18n.t("enums.receipt.payment_method.#{payment_method}")
+  end
 end
