a4aeee1a29a12ba55227daac9407d4827fc154bf
matsumoto <255082730+matsumoto219@users.noreply.github.com>
2026-04-16 01:45:57 +0900

    feat: deviseコントローラを作成し、routesに追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+    passwords: "users/passwords"
+  }
   post "/users/guest_sign_in", to: "guest_sessions#create", as: :guest_sign_in
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
