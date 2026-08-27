b6f728ae993a11a6f6e2b5cdd1f6f04970767f7b
matsumoto <255082730+matsumoto219@users.noreply.github.com>
2026-08-28 04:23:19 +0900

    test: 計算方式選択のレイアウト回帰を追加

--- spec/tools/generated_receipts/text_renderer_spec.rb
+++ spec/tools/generated_receipts/text_renderer_spec.rb
@@ -18,8 +18,8 @@ RSpec.describe GeneratedReceipts::TextRenderer do
     aggregate_failures do
       expect(GeneratedReceipts.legacy_case_paths.size).to eq(112)
       expect(GeneratedReceipts.measurement_case_paths.size).to eq(10)
-      expect(GeneratedReceipts.calculation_mode_case_paths.size).to eq(23)
-      expect(GeneratedReceipts.case_paths.size).to eq(145)
+      expect(GeneratedReceipts.calculation_mode_case_paths.size).to eq(29)
+      expect(GeneratedReceipts.case_paths.size).to eq(151)
     end
 
     GeneratedReceipts.case_paths.each do |path|
